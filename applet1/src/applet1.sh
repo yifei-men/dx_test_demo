0a3cc7c61041bdf0720da47ba1db1888dd59f99a
Marcus Kinsella <mkinsella@dnanexus.com>
2015-10-29 11:30:22 -0700

    Screw around with BWA parameters
    This is just to get some different numbers pushed to keen.

--- applet1/src/applet1.sh
+++ applet1/src/applet1.sh
@@ -10,7 +10,7 @@ main() {
     genome_file=`ls genome/*.bwt`
     genome_file="${genome_file%.bwt}"
 
-    bwa mem -t `nproc` "$genome_file" input.fastq | samtools view -u -S - | samtools sort -m 256M -@ `nproc` - output
+    bwa mem -t `nproc` -T 35 -O 3 "$genome_file" input.fastq | samtools view -u -S - | samtools sort -m 256M -@ `nproc` - output
 
     bam=$(dx upload output.bam --brief)
 
